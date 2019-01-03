92e42246b7e985d24c5000a33dd9ab5a7c15a2ef
Jenny <yjhong1004@gmail.com>
2019-01-02 17:42:28 -0800

    retrieving users first changes

--- lib/kele.rb
+++ lib/kele.rb
@@ -9,15 +9,19 @@ class Kele
   end
   def initialize(email, password)
     @options = {
-      body: {
       email: email,
       password: password
-      }
+
     }
     response = self.class.post("#{base_uri}/sessions", @options)
-    @auth_token = response['auth_token']
+    @auth_token = response["auth_token"]
     if @auth_token.nil?
       puts "Sorry, invalid credentials."
     end
+  end
+
+  def get_me
+    response = self.class.get("#{base_uri}/users/me", headers: { "authorization" => @auth_token })
+    JSON.parse(response.body)
   end
 end
