bae5eecfc6afb16cf90979576faa7bb3d27fe425
Jenny <yjhong1004@gmail.com>
2019-01-02 22:28:47 -0800

    mentor availabilities first changes

--- lib/kele.rb
+++ lib/kele.rb
@@ -7,6 +7,7 @@ class Kele
   def base_uri
     'https://www.bloc.io/api/v1'
   end
+
   def initialize(email, password)
     @options = {
       email: email,
@@ -24,4 +25,10 @@ class Kele
     response = self.class.get("#{base_uri}/users/me", headers: { "authorization" => @auth_token })
     JSON.parse(response.body)
   end
+
+  def get_mentor_availability(mentor_id)
+    response = self.class.get("#{base_uri}/mentors/#{mentor_id}/student_availability", headers: {"authorization" => @auth_token} )
+    JSON.parse(response.body)
+  end
+
 end
